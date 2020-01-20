fd22675a1e63ca68fed7dc5bbd7992bcac285846
Dmytro9 <dpoliuliuk@gmail.com>
2020-01-20 22:29:58 +0200

    udemy

---
+++ udemy_lessons/if_statement.py
@@ -0,0 +1,25 @@
+if True:
+  print("Something")
+
+if 3 > 2:
+  print("Yes")
+
+
+num1 = 100
+num2 = 50
+
+if num1 > num2:
+  print("num1 > num2")
+else:
+  print("num2 > num1")
+
+
+num1 = 100
+num2 = 100
+
+if num1 > num2:
+  print("num1 > num2")
+elif num2 > num1:
+  print("num2 > num1")
+else:
+  print("num2 = num1")
---
+++ udemy_lessons/object_and_classes.py
@@ -0,0 +1,6 @@
+# Class is the template for creating objects
+# to create repetetive instances
+
+# Class
+## states - methods
+### value/color/diameter - flip()/some()
